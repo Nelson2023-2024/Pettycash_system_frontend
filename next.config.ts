4bc1d38ef183b87657343bfdbd8e86146548c591
Nelson2023-2024 <nelsonobuya19@gmail.com>
2026-03-05 15:28:59 +0300

    feat: configure API rewrites to proxy requests to Django backend

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*", // any /api/... call in your Next.js app
+        destination:`${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,  // forwarded to Django
+
+      }
+    ]
+  },
 };
 
 export default nextConfig;
