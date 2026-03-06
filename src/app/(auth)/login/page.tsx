2cc1798568789e1d628389b89baaa509efe9030e
Nelson2023-2024 <nelsonobuya19@gmail.com>
2026-03-06 13:33:36 +0300

    feat(auth): implement login page and login form UI
    - create LoginPage component
    - add LoginForm component
    - integrate shadcn/ui components (Card, Button, Input, Field)
    - add social login buttons UI
    - include responsive layout with image section

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,11 @@
+import { LoginForm } from "@/components/login-form";
 
-
-const LoginPage = () => {
+export default function LoginPage() {
   return (
-    <div>LoginPage</div>
-  )
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-4xl">
+        <LoginForm />
+      </div>
+    </div>
+  );
 }
-
-export default LoginPage
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+
 
 export default function Home() {
- return(
-  <>
-  <h1><Button variant={"default"}>Hello world</Button></h1>
-  <input type="text" />
-  </>
- )
+  return (
+    <>
+     <h1>Home</h1>
+    </>
+  );
 }
