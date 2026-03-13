e30ba59f3fd0ed5d129efcd3bef9ea89ed4cd71f
Nelson2023-2024 <nelsonobuya19@gmail.com>
2026-03-13 08:35:40 +0300

    feat(department): component select to retrieve all departments

---
+++ src/components/departments/department-select.tsx
@@ -0,0 +1,48 @@
+"use client";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useGetAllDepartments } from "@/hooks/useDepartment";
+import { Spinner } from "../ui/spinner";
+
+interface DepartmentSelectProps {
+  value?: string;
+  onChange: (id: string) => void;
+  placeholder?: string;
+}
+
+export function DepartmentSelect({
+  value,
+  onChange,
+  placeholder = "select a department",
+}: DepartmentSelectProps) {
+  const { data: departments = [], isPending } = useGetAllDepartments();
+  return (
+    <Select value={value} onValueChange={onChange} disabled={isPending}>
+      <SelectTrigger>
+        <SelectValue
+          placeholder={
+            isPending ? (
+              <>
+                <Spinner /> Loading....
+              </>
+            ) : (
+              placeholder
+            )
+          }
+        />
+      </SelectTrigger>
+      <SelectContent>
+        {departments.map((department) => (
+          <SelectItem key={department.id} value={department.id}>
+            {department.name}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+}
