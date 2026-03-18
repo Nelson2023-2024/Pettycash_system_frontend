e5f3e6b7fa9677f92d75a26968d5e9757703d5ad
Nelson2023-2024 <nelsonobuya19@gmail.com>
2026-03-18 10:58:25 +0300

    feat(next.config): add Cloudinary remote image patterns and clean rewrites formatting

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,27 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+    ],
+  },
   async rewrites() {
     return [
       {
         source: "/api/:path*", // any /api/... call in your Next.js app
-        destination:`${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,  // forwarded to Django
-
-      }
-    ]
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`, // forwarded to Django
+      },
+    ];
   },
 };
 
