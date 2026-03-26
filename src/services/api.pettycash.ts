19915f94eb58b53461338ebd4dcd887c57d366f2
Nelson2023-2024 <nelsonobuya19@gmail.com>
2026-03-26 16:08:30 +0300

    feat(api): add petty cash activity export endpoint with blob response
    - add exportPettyCashActivity API (weekly/monthly)
    - configure axios to handle file download (responseType: blob)
    - format API functions for better readability

--- src/services/api.pettycash.ts
+++ src/services/api.pettycash.ts
@@ -1,30 +1,44 @@
-
-
-
 import { axiosInstance } from "@/lib/axios";
 import { ApiResponse } from "@/types/common";
-import { CreatePettyCashPayload, PettyCash, PettyCashAccountActivity, UpdatePettyCashPayload } from "@/types/pettycash";
+import {
+  CreatePettyCashPayload,
+  PettyCash,
+  PettyCashAccountActivity,
+  UpdatePettyCashPayload,
+} from "@/types/pettycash";
 
 // FO
 export const getPettyCashAccounts = () =>
   axiosInstance.get<ApiResponse<PettyCash[]>>("/finance/petty_cash/");
 
-
 export const getPettyCashById = (id: string) =>
   axiosInstance.get<ApiResponse<PettyCash>>(`/finance/petty_cash/${id}/`);
 
-
 // ADMIN, CFO
 export const createPettyCash = (payload: CreatePettyCashPayload) =>
-  axiosInstance.post<ApiResponse<PettyCash>>("/finance/petty_cash/create/", payload,{
-  });
+  axiosInstance.post<ApiResponse<PettyCash>>(
+    "/finance/petty_cash/create/",
+    payload,
+    {},
+  );
 
 export const updatePettyCash = (id: string, payload: UpdatePettyCashPayload) =>
-  axiosInstance.patch<ApiResponse<PettyCash>>(`/finance/petty_cash/${id}/update/`, payload);
+  axiosInstance.patch<ApiResponse<PettyCash>>(
+    `/finance/petty_cash/${id}/update/`,
+    payload,
+  );
 
 export const deactivatePettyCash = (id: string) =>
-  axiosInstance.delete<ApiResponse<PettyCash>>(`/finance/petty_cash/${id}/deactivate/`);
+  axiosInstance.delete<ApiResponse<PettyCash>>(
+    `/finance/petty_cash/${id}/deactivate/`,
+  );
 
 export const getPettyCashActivity = () =>
-  axiosInstance.get<ApiResponse<PettyCashAccountActivity>>("/finance/petty_cash/activity/");
+  axiosInstance.get<ApiResponse<PettyCashAccountActivity>>(
+    "/finance/petty_cash/activity/",
+  );
 
+export const exportPettyCashActivity = (period: "weekly" | "monthly") =>
+  axiosInstance.get(`/finance/petty_cash/activity/export/?period=${period}`, {
+    responseType: "blob",
+  });
